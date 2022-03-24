f1b202360ae7f6173d6afdb910c0b8af4fb2816c
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-03-23 17:13:44 -0700

    fix: use hosted image

--- create-conflict-app/bot/commands/help.js
+++ create-conflict-app/bot/commands/help.js
@@ -12,7 +12,7 @@ export default new Command({
     execute: async (command, options, utils) => {
         const buttonView = (
             <message>
-                <Embed color="#ff3333" image={{ url: "https://www.gitbook.com/cdn-cgi/image/width=32,height=32,fit=contain,dpr=1,format=auto/https%3A%2F%2Ffiles.gitbook.com%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FqKkjNKHtBhCUHveEGTY2%252Ficon%252F2sgixS2pVqtWMiBvRGg2%252FConflict.png%3Falt%3Dmedia%26token%3D24c0b0f6-f987-46ca-b4a5-8f193d650bcb" }}>
+                <Embed color="#ff3333" image={{ url: "https://conflict.js.org/favicon.png" }}>
                         <title>Hello, world!</title>
                         <description>Welcome to **Conflict**.</description>
                 </Embed>
