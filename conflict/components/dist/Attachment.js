91bbd3e6f8e2f0ced07f79c80d2738355d5674aa
lumins <luminsx2@gmail.com>
2023-02-14 08:00:59 -0500

    Replace `Babel` with `SWC` for faster build times. (#1)
    * Remove `Babel` from the `conflict` directory.
    
    * test

--- conflict/components/dist/Attachment.js
+++ conflict/components/dist/Attachment.js
@@ -1,15 +1,24 @@
 export default function Attachment({
-  url,
-  attachment,
-  name,
-  description,
-  children
+	url,
+	attachment,
+	name,
+	description,
+	children,
 }) {
-  if (url) {
-    return global.__ConflictViewParser("files_arr", null, url);
-  } else return global.__ConflictViewParser("files_arr", null, global.__ConflictViewParser("file", {
-    description,
-    name,
-    attachment
-  }, children));
+	if (url) {
+		return global.__ConflictViewParser("files_arr", null, url);
+	} else
+		return global.__ConflictViewParser(
+			"files_arr",
+			null,
+			global.__ConflictViewParser(
+				"file",
+				{
+					description,
+					name,
+					attachment,
+				},
+				children
+			)
+		);
 }
--- conflict/components/dist/Modal.js
+++ conflict/components/dist/Modal.js
@@ -1,14 +1,13 @@
-import { managers } from '../../state.js';
-export default function Modal({
-  children,
-  onsubmit,
-  onSubmit,
-  title
-}) {
-  if (!onsubmit && onSubmit) onsubmit = onSubmit;
-  if (!onsubmit) throw new Error('Modal must have onsubmit prop');
-  return global.__ConflictViewParser("modal", {
-    title: title,
-    custom_id: managers.components.select('*').store(onsubmit)
-  }, children);
+import { managers } from "../../state.js";
+export default function Modal({ children, onsubmit, onSubmit, title }) {
+	if (!onsubmit && onSubmit) onsubmit = onSubmit;
+	if (!onsubmit) throw new Error("Modal must have onsubmit prop");
+	return global.__ConflictViewParser(
+		"modal",
+		{
+			title: title,
+			custom_id: managers.components.select("*").store(onsubmit),
+		},
+		children
+	);
 }
